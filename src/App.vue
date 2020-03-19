be48d8fa39c741fabd623ca5e446f790e90a2b19
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-03-19 18:51:16 +0530

    add basic details

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,146 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="content flex space-between">
+        <div class="w-6/12 flex flex-col">
+          <div class="w-12 h-12 flex justify-center items-center bg-indigo-500 border-indigo-500 rounded-full">
+            <p class="text-white mt-4 text-3xl">DB</p>
+          </div>
+          <p class="pt-4 ml-2 text-2xl">
+            Deepak Bhardwaj
+            <!-- <i class="text-sm text-gray-700 cursor-pointer m-2 fas fa-map-pin"></i>
+            INDIA
+            <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-code"></i>
+            Computer Science -->
+          </p>
+          <hr>
+          <div class="">
+            <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-js"></i>
+            <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-vuejs"></i>
+            <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-react"></i>
+            <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-node"></i>
+          </div>
+          <!-- <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-chrome"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-mobile"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-angellist"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-award"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-prescription"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-heartbeat"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-graduation-cap"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-school"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-code"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-box-open"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-birthday-cake"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-baby"></i>
+          <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-map-pin"></i> -->
+          <!-- #B4C873 -->
+          <hr>
+          <div class="">
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-film"></i>
+              <p>vidly app</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-code-branch"></i>
+              <p>Git client | Electron</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-list-ol"></i>
+              <p>Todo | using trello api</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-images"></i>
+              <p>Giflib</p>
+            </div>
+            <div class="flex items-start">
+              <!-- <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-toggle-on"></i> -->
+              <img class="w-6 cursor-pointer m-2" src="./assets/img/coheto.png" alt="coheto">
+              <div class="flex-col">
+                <p>CoHeTo</p>
+                <p>I can help you go in presentation mode in Codepen. It gives you some extra space to work :)</p>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fab fa-buromobelexperte"></i>
+              <p>FlexBox Visualiser</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 far fa-id-badge"></i>
+              <p>Contact App</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-stopwatch"></i>
+              <p>Counter App</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-palette"></i>
+              <p>Color guessing game</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-dice"></i>
+              <p>Score keeper game</p>
+            </div>
+            <hr>
+            
+          </div>
+
+          <div class="">
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-check-double"></i>
+              <p>Checkbox app | Subscription tracker</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-home"></i>
+              <p>81Hut</p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-trailer"></i>
+              <p>leave application chrome extension </p>
+            </div>
+            <div class="flex items-center">
+              <i class="text-2xl text-gray-700 cursor-pointer m-2 fas fa-sitemap"></i>
+              <p>DP Visualiser</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="flex items-center flex-wrap pl-4 w-6/12 rounded-md bg-blue-200">
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-github"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-facebook"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-instagram"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-linkedin"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-medium"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-codepen"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-twitter"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-youtube"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-twitch"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-snapchat"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-skype"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-paypal"></i>
+          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-patreon"></i>
+        </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 80%;
+  margin: 50px auto;
 }
+body {
+  background-color: #ebf8ff;
+  font-family: 'Baloo Tammudu 2', cursive;
+}
+
 </style>
