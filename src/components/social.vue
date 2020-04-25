b9f2561a801bc61965b1f4883309eda4f84d9596
Deepak Bhardwaj <kapeed07@gmail.com>
2020-04-25 22:03:04 +0530

    try new look

--- src/components/social.vue
+++ src/components/social.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="flex items-center flex-wrap pl-4 w-6/12 rounded-md bg-blue-200">
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-github"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-facebook"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-instagram"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-linkedin"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-medium"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-codepen"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-twitter"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-youtube"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-twitch"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-snapchat"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-skype"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-paypal"></i>
-          <i class="text-2xl text-indigo-700 cursor-pointer m-2 fab fa-patreon"></i>
+  <div class="flex items-center flex-wrap rounded-md">
+          <i class="text-2xl cursor-pointer m-2 fab fa-github"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-facebook"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-instagram"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-linkedin"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-medium"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-codepen"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-twitter"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-youtube"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-twitch"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-snapchat"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-skype"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-paypal"></i>
+          <i class="text-2xl cursor-pointer m-2 fab fa-patreon"></i>
         </div>
 </template>
