c3e441f88fbdce85c324ab466b1043a994e6546c
Deepak Bhardwaj <kapeed07@gmail.com>
2020-04-26 14:33:03 +0530

    change in design

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div class="curved flex flex-col  justify-center items-center">
-      <h2 class="text-3xl text-white">Deepak Bhardwaj</h2>
+    <!-- header -->
+    
+    <!-- content -->
+    <div class="flex flex-col items-center justify-center">
+      <p class="text-2xl">Deepak Bhardwaj</p>
       <social></social>
+      <skills></skills>
+      <div v-for="project in projects" :key="project.id">
+        <a href="">
+          <p :class="[project.status ? '' : 'line-through text-gray-300']">{{ project.title }}</p>
+        </a>
+      </div>
     </div>
-    <div class="flex flex-col h-full items-center justify-between">
-      <div class="content m-10">
-      <project-card v-for="project in projects" :project="project" :key="project.id"></project-card>
-
-      </div>
-
-      <div class="footer flex justify-center items-center text-white w-screen h-20">
-        <p>
-        Made with <i class="text-red-600 fas fa-heart"></i> in <span class="text-green-600 font-medium">VueJS </span>
-        </p>
-        <p class="pl-3">
-        Hosted on <i class="text-white fab fa-github"></i>
-        </p>
-      </div>
+    
+    <!-- footer -->
+    <div class="flex items-center justify-center">
+      <p>Hosted on <i class="fab fa-github"></i></p>
     </div>
   </div>
 </template>
 
 <script>
-import ProjectCard from "@/components/project-card";
 import Social from "@/components/social";
-// import Skills from "@/components/skills";
-// import DbHeader from "@/components/header";
+import Skills from "@/components/skills";
 
 export default {
   name: "app",
@@ -40,7 +37,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "green",
           shade: "500",
-          icon: "fas fa-toggle-on"
+          icon: "fas fa-toggle-on",
+          status: 1
         }, {
           id: 2,
           logo: "",
@@ -48,7 +46,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "blue",
           shade: 500,
-          icon: "far fa-id-badge"
+          icon: "far fa-id-badge",
+          status: 1
         }, {
           id: 3,
           logo: "",
@@ -56,7 +55,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "pink",
           shade: 500,
-          icon: "fas fa-images"
+          icon: "fas fa-images",
+          status: 0
         }, {
           id: 4,
           logo: "",
@@ -64,7 +64,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "indigo",
           shade: 500,
-          icon: "fas fa-stopwatch"
+          icon: "fas fa-stopwatch",
+          status: 1
         }, {
           id: 5,
           logo: "",
@@ -72,7 +73,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "red",
           shade: 500,
-          icon: "fab fa-buromobelexperte"
+          icon: "fab fa-buromobelexperte",
+          status: 1
         }, {
           id: 6,
           logo: "",
@@ -80,7 +82,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "orange",
           shade: 500,
-          icon: "fas fa-dice"
+          icon: "fas fa-dice",
+          status: 1
         }, {
           id: 7,
           logo: "",
@@ -88,7 +91,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "teal",
           shade: 500,
-          icon: "fas fa-palette"
+          icon: "fas fa-palette",
+          status: 1
         }, {
           id: 8,
           logo: "",
@@ -96,7 +100,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "orange",
           shade: 500,
-          icon: "fas fa-stopwatch"
+          icon: "fas fa-stopwatch",
+          status: 0
         }, {
           id: 9,
           logo: "",
@@ -104,7 +109,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "blue",
           shade: 500,
-          icon: "fas fa-check-double"
+          icon: "fas fa-check-double",
+          status: 0
         }, {
           id: 10,
           logo: "",
@@ -112,7 +118,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "pink",
           shade: 500,
-          icon: "fas fa-home"
+          icon: "fas fa-home",
+          status: 0
         }, {
           id: 11,
           logo: "",
@@ -120,7 +127,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "red",
           shade: 500,
-          icon: "fas fa-list-ol"
+          icon: "fas fa-list-ol",
+          status: 0
         }, {
           id: 12,
           logo: "",
@@ -128,7 +136,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "green",
           shade: 500,
-          icon: "fas fa-code-branch"
+          icon: "fas fa-code-branch",
+          status: 0
         }, {
           id: 13,
           logo: "",
@@ -136,7 +145,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "pink",
           shade: 500,
-          icon: "fas fa-film"
+          icon: "fas fa-film",
+          status: 0
         }, {
           id: 14,
           logo: "",
@@ -144,7 +154,8 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "yellow",
           shade: 500,
-          icon: "fas fa-trailer"
+          icon: "fas fa-trailer",
+          status: 0
         }, {
           id: 15,
           logo: "",
@@ -152,73 +163,28 @@ export default {
           desc: "CoHeTo helps you go in presentation mode on Codepen and provides you some extra space to work with.",
           color: "purple",
           shade: 500,
-          icon: "fas fa-sitemap"
+          icon: "fas fa-sitemap",
+          status: 0
         }
-      ],
-      searchTerm: ""
+      ]
     }
   },
-  computed: {
-    // filteredProjects() {
-    //   return this.projects.filter(project => project.title.toLowerCase().includes(this.searchTerm.toLowerCase()));
-    // }
-  },
+  created() {},
+  mounted() {},
+  computed: {},
+  watch: {},
   components: {
-    ProjectCard,
     Social,
-    // Skills,
-    // DbHeader
+    Skills
   }
 };
 </script>
 
 <style>
-
 * {
   padding: 0;
   margin: 0;
-}
-.content {
-  padding-top: 200px;
-}
-#app {
-  height: 100%;
-}
-.db-text-black {
-  color: #363537;
-}
-.curved {  
-  position: fixed;
-  width: 100%;
-  z-index: 1;
-  background: #DA3E52;
-  /* filter: drop-shadow(2px 4px 1px #8cd867); */
-  height: 200px;
-  border-bottom-left-radius: 50% 30%;
-  border-bottom-right-radius: 50% 30%;
-}
-.profile-image {
-  filter: drop-shadow(4px 7px 5px #000);
-}
-.footer {
-  background: #363537;
-  /* position: absolute; */
-  width: 100%;
-  bottom: 0;
-}
-
-.box {
-
-  border-left: 5px dashed #ef2d56;
-  border-bottom: 5px dashed #ef2d56;
-  
-}
-
-body {
-  padding: 0;
-  margin: 0;
-  /* background-color: #ebf8ff; */
+  outline: none;
   font-family: 'Nunito', sans-serif;
 }
-/* #7034AC */
 </style>
--- src/components/social.vue
+++ src/components/social.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="flex items-center flex-wrap rounded-md">
-          <i class="text-2xl cursor-pointer m-2 fab fa-github"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-facebook"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-instagram"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-linkedin"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-medium"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-codepen"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-twitter"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-youtube"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-twitch"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-snapchat"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-skype"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-paypal"></i>
-          <i class="text-2xl cursor-pointer m-2 fab fa-patreon"></i>
-        </div>
+  <div class="flex w-40 items-center flex-wrap">
+    <a href="https://linkedin.com/in/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-linkedin"></i>
+    </a>
+    <a href="https://instagram.com/heykapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-instagram"></i>
+    </a>
+    <a href="https://github.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-github"></i>
+    </a>
+    <a href="https://facebook.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-facebook"></i>
+    </a>
+
+    <a href="https://medium.com/@kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-medium"></i>
+    </a>
+    <a href="https://codepen.io/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-codepen"></i>
+    </a>
+    <a href="https://twitter.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-twitter"></i>
+    </a>
+    <a href="https://twitch.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-twitch"></i>
+    </a>
+
+    <a href="https://dev.to/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-dev"></i>
+    </a>
+    <a href="https://dribbble.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-dribbble"></i>
+    </a>
+    <a href="https://patreon.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fab fa-patreon"></i>
+    </a>
+    <a href="https://open.spotify.com/user/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fa fa-spotify"></i>
+    </a>
+
+    <a href="https://leetcode.com/kapeed07" target="blank">
+      <i class="text-2xl cursor-pointer m-2 fa fa-terminal"></i>
+    </a>
+
+    <i class="text-2xl text-gray-300 m-2 fab fa-snapchat"></i>
+    <i class="text-2xl text-gray-300 m-2 fab fa-youtube"></i>
+    <i class="text-2xl text-gray-300 m-2 fab fa-skype"></i>
+    <i class="text-2xl text-gray-300 m-2 fab fa-paypal"></i>
+  </div>
 </template>
